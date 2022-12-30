8744d53987930ee09417a3e1e6f057b0c3677743
Hadrien David <hadrien.david@dialogue.co>
2022-12-29 20:06:43 -0500

    chore: just backing up yo

--- raoul.py
+++ raoul.py
@@ -1,10 +1,23 @@
+import asyncio
+import subprocess
+import sys
+
+
 import structlog
-from fastapi import FastAPI, WebSocket
-from starlette.websockets import WebSocketDisconnect
+from fastapi import FastAPI, Response
+from starlette.responses import StreamingResponse
+from starlette.staticfiles import StaticFiles
+from starlette.websockets import WebSocket, WebSocketDisconnect
 
 
 app = FastAPI()
+queue = None
+ffmpeg = None
 log = structlog.get_logger()
+FRAME_SEPARATOR = b"\xff\xd8"
+BUFFER_SIZE = 65536
+
+app.mount("/static", StaticFiles(directory="static", html=True), name="static")
 
 
 @app.get("/health")
@@ -12,17 +25,74 @@ def health():
     return "ok"
 
 
+@app.on_event("startup")
+async def on_startup():
+    global queue, ffmpeg
+    queue = asyncio.Queue()
+    cmd = "ffmpeg -f mpegts -c:v h264 -i - -c:v libvpx-vp9 -f webm -"
+    # ffmpeg = await asyncio.create_subprocess_shell(
+    #    cmd,
+    #    stdin=subprocess.PIPE,
+    #    stdout=subprocess.PIPE,
+    #    stderr=sys.stdout,
+    #    limit=BUFFER_SIZE,
+    # )
+
+
+gws = None
+
+
 @app.websocket("/ws")
-async def websocket(websocket: WebSocket):
-    await websocket.accept()
+async def websocket(ws: WebSocket):
+    await ws.accept()
+    global gws
+    gws = ws
     log.info("Accepting connection")
     while True:
         try:
-            data = await websocket.receive_bytes()
-
+            data = await ws.receive_bytes()
         except WebSocketDisconnect:
             log.info("Closing connection", exc_info=True)
             break
 
         else:
-            log.debug("Receiving", len=len(data))
+            await queue.put(data)
+    log.info("over")
+
+
+async def iter_jpeg_frames(queue):
+    data = await queue.get() + await queue.get() + await queue.get() + await queue.get()
+    log.debug(data[:30])
+    log.debug("getting frames")
+    first_frame_index = data.index(FRAME_SEPARATOR)
+    data = data[first_frame_index:]
+    while True:
+        frame_count = data.count(FRAME_SEPARATOR)
+        for _ in range(frame_count - 1):
+            end = data.index(FRAME_SEPARATOR, len(FRAME_SEPARATOR))
+            frame = data[0:end]
+            yield frame
+            data = data[end:]
+        data = data + await queue.get()
+
+
+async def iter_multipart_frames(queue):
+    async for frame in iter_jpeg_frames(queue):
+        yield b"--FRAME\r\n"
+        yield b"Content-Type: image/jpeg\r\n"
+        yield f"Content-Length {len(frame)}\r\n\r\n".encode()
+        yield frame
+        yield b"\r\n"
+
+
+@app.get("/video")
+async def mjpeg(response: Response):
+    headers = {
+        "Age": "0",
+        "Cache-Control": "no-cache, private",
+        "Pragma": "no-cache",
+        "Content-Type": "multipart/x-mixed-replace; boundary=FRAME",
+    }
+
+    await gws.send_bytes(b"yolo")
+    return StreamingResponse(iter_multipart_frames(queue), headers=headers)
--- raoul_client.py
+++ raoul_client.py
@@ -1,85 +1,55 @@
-import os
-import socket
+import asyncio
 import subprocess
 import sys
-import tempfile
-from concurrent import futures
 
 import structlog
+import websockets
 
 log = structlog.get_logger()
 
-cmd = "libcamera-vid -v -t 0 --inline --framerate 60 -o -"
-# cmd = "find ."
+cmd = "libcamera-vid -t 0 --codec mjpeg --framerate 30 -q 100 -n -o -"
 
-BUFFER_SIZE = 16_384
-
-
-def new_socket_address():
-    fd, path = tempfile.mkstemp()
-    os.close(fd)
-    os.unlink(path)
-    return path
+BUFFER_SIZE = 65_536
 
 
-def handle_video_stream(socket_address, consume):
-    log.debug("Creating unix socket", socket_address=socket_address)
-    video_stream = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-    video_stream.bind(socket_address)
-    video_stream.listen(1)
+async def run():
+    async with websockets.connect(
+        "ws://C02CM2JHMD6P-Hadrien-David.local:8000/ws"
+    ) as ws:
+        async with CameraCapture(ws) as capture:
+            while True:
+                await ws.recv()
+                capture.on = True
+                await ws.recv()
 
-    log.debug("Waiting for connection")
-    connection, _ = video_stream.accept()
-    log.debug("Connecting")
-    try:
+
+class CameraCapture:
+    def __init__(self, ws):
+        self.on = False
+        self.ws = ws
+
+    async def __aenter__(self):
+        self.process = await asyncio.create_subprocess_shell(
+            cmd, stdout=subprocess.PIPE, stderr=sys.stdout, limit=BUFFER_SIZE
+        )
+        await self.process.stdout.read(BUFFER_SIZE)
+        self._capture_task = asyncio.create_task(self._capture())
+        log.info("Running libcamera", cmd=cmd)
+        return self
+
+    async def _capture(self):
         while True:
-            data = connection.recv(BUFFER_SIZE)
-            if data:
-                log.debug("Receiving", len=len(data))
-                consume(data)
-            else:
-                break
+            data = await self.process.stdout.read(BUFFER_SIZE)
+            if self.on:
+                await self.ws.send(data)
 
-    finally:
-        log.info("End")
-        connection.close()
-        video_stream.close()
+    async def __aexit__(self, exc_type, exc, tb):
+        self._capture_task.cancel()
 
 
-class Consumer:
-    def __init__(self):
-        self.ws = websocket.WebSocket()
-        self.ws.connect("ws://C02CM2JHMD6P-Hadrien-David.local:8000/ws")
-
-    def __call__(self, data: bytes):
-        self.ws.send_binary(data)
-
-
-def run_libcamera_vid(socket_address):
-    client = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-    while True:
-        try:
-            client.connect(socket_address)
-        except (ConnectionRefusedError, FileNotFoundError):
-            pass
-        else:
-            break
-
-    subprocess.run(
-        cmd.split(),
-        stdout=client.makefile(mode="wb"),
-        stderr=sys.stdout,
-        check=True,
-    )
-
-
-def run():
-    executor = futures.ThreadPoolExecutor(max_workers=1)
-    socket_address = new_socket_address()
-
-    executor.submit(handle_video_stream, socket_address, Consumer())
-    run_libcamera_vid(socket_address)
+def main():
+    asyncio.run(run())
 
 
 if __name__ == "__main__":
-    run()
+    main()
